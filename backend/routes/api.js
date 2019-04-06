a40d6e897eea30626e154af98d2c71738176931a
Mauricio Ochoa <mauri_8a_flores@hotmail.com>
2019-04-06 03:59:07 -0600

    Terminando carrito

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -5,10 +5,12 @@ function apiInit(db){
     var usersApi = require('./api/users');
     var encuestaApi = require('./api/encuesta')(db);
     var catApi = require('./api/catalogo')(db);
+    var cartApi = require('./api/Carrito/carrito')(db);
 
     router.use('/users',usersApi);
     router.use('/catalogo',catApi);
     router.use('/encuesta',encuestaApi);
+    router.use('/carrito', cartApi);
 
     return router;
 }
